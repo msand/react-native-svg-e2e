3ed24893025a497e340c21cc26b074f2525eed98
Mikael Sand <msand@abo.fi>
2019-11-04 21:48:21 +0200

    Upgrade to react-native 0.60.6

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -9,17 +9,6 @@
 
 # Add any project specific keep options here:
 
-# If your project uses WebView with JS, uncomment the following
-# and specify the fully qualified class name to the JavaScript interface
-# class:
-#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
-#   public *;
-#}
-#
-# Most needed rules come from React Native's proguard-rules.pro (either .aar or source) -- hence
-# the lean configuration file.
-###
-
 -dontobfuscate
 
 -dontnote android.net.**
--- android/app/src/main/java/com/reactnativesvge2e/MainApplication.java
+++ android/app/src/main/java/com/reactnativesvge2e/MainApplication.java
@@ -1,7 +1,11 @@
 package com.reactnativesvge2e;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -22,10 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SvgPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
     }
 
     @Override
